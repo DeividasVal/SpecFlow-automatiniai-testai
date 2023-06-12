767cf44134a3cf8f19881081c209d0d41228ea22
DeividasVal <105603552+DeividasVal@users.noreply.github.com>
2023-06-12 17:03:36 +0300

    Add files via upload

---
+++ EasyScanSpecflow/Drivers/AppiumDriver.cs
@@ -0,0 +1,46 @@
+﻿using EasyScanSpecflow.StepLogging;
+using Microsoft.Extensions.Configuration;
+using NUnit.Framework;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Android;
+using OpenQA.Selenium.Appium.Enums;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyScanSpecflow.Drivers
+{
+    public class AppiumDriver
+    {
+        private static readonly Lazy<AndroidDriver<AppiumWebElement>> _currentAppiumDriverLazy =
+            new Lazy<AndroidDriver<AppiumWebElement>>(InitializeAppium);
+
+        public static AndroidDriver<AppiumWebElement> Current => _currentAppiumDriverLazy.Value;
+        private AppiumDriver(){}
+
+        private static AndroidDriver<AppiumWebElement> InitializeAppium()
+        {
+            var driverOptions = new AppiumOptions();
+
+            driverOptions.AddAdditionalCapability(MobileCapabilityType.PlatformName, Config.configuration["PlatformName"]);
+            //driverOptions.AddAdditionalCapability(MobileCapabilityType.PlatformVersion, Config.configuration["PlatformVersion"]);
+            driverOptions.AddAdditionalCapability("appPackage", Config.configuration["AppPackage"]); //comment out if testing settings
+            driverOptions.AddAdditionalCapability("appActivity", Config.configuration["AppActivity"]); //comment out if testing settings
+            driverOptions.AddAdditionalCapability("noReset", true);
+            driverOptions.AddAdditionalCapability("disableWindowAnimation", true);
+
+            try
+            {
+                return new AndroidDriver<AppiumWebElement>(new Uri("http://localhost:4723/wd/hub"), driverOptions);
+            }
+            catch (Exception ex)
+            {
+                LogWriter.LogWrite(ex.ToString());
+                return null;
+            }
+        }
+    }
+}
